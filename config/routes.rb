f01608c1ca10ba66ba5f490bb345444dc8066947
Majd Murad <murad.majd@gmail.com>
2016-12-28 14:32:10 -0800

    Completed Challenge

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,4 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "main#index"
-  get '/item/:type', to: 'main#item'
-  get '/art_resize/:size', to: 'main#art_resize'
 end
